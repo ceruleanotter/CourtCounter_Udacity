0aca996a0a4e597941662323c6887fcb430c6a5f
Lyla <lyla@udacity.com>
2015-05-14 13:34:04 -0700

    Update with the other buttons

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -44,14 +44,16 @@ public class MainActivity extends AppCompatActivity {
    * Increase the score for Team A by 1 point.
    */
   public void addOneForTeamA(View v) {
-    displayForTeamA(1);
+    scoreTeamA = scoreTeamA + 1;
+    displayForTeamA(scoreTeamA);
   }
 
   /**
    * Increase the score for Team A by 2 points.
    */
   public void addTwoForTeamA(View v) {
-    displayForTeamA(2);
+    scoreTeamA = scoreTeamA + 2;
+    displayForTeamA(scoreTeamA);
   }
 
   /**
