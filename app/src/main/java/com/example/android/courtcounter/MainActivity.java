0e15d2f18bb4aac8b80f33b2b17d592fb9591625
Lyla <lyla@udacity.com>
2015-05-14 13:27:46 -0700

    Setting up the methods

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +35,34 @@ public class MainActivity extends ActionBarActivity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  /**
+   * Increase the score for Team A by 1 point.
+   */
+  public void addOneForTeamA(View v) {
+    displayForTeamA(1);
+  }
+
+  /**
+   * Increase the score for Team A by 2 points.
+   */
+  public void addTwoForTeamA(View v) {
+    displayForTeamA(2);
+  }
+
+  /**
+   * Increase the score for Team A by 3 points.
+   */
+  public void addThreeForTeamA(View v) {
+    displayForTeamA(3);
+  }
+
+  /**
+   * Displays the given score for Team A.
+   */
+  public void displayForTeamA(int score) {
+    TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+    scoreView.setText(String.valueOf(score));
   }
 }
