44bdf47a6df7a1b45ad0513d588c9101d4aa6ed1
Lyla <lyla@udacity.com>
2015-05-14 13:41:19 -0700

    Add the other team in Java

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
   // Tracks the score for Team A
   int scoreTeamA = 0;
 
+  // Tracks the score for Team B
+  int scoreTeamB = 0;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -65,10 +68,42 @@ public class MainActivity extends AppCompatActivity {
   }
 
   /**
+   * Increase the score for Team B by 1 point.
+   */
+  public void addOneForTeamB(View v) {
+    scoreTeamB = scoreTeamB + 1;
+    displayForTeamB(scoreTeamB);
+  }
+
+  /**
+   * Increase the score for Team B by 2 points.
+   */
+  public void addTwoForTeamB(View v) {
+    scoreTeamB = scoreTeamB + 2;
+    displayForTeamB(scoreTeamB);
+  }
+
+  /**
+   * Increase the score for Team B by 3 points.
+   */
+  public void addThreeForTeamB(View v) {
+    scoreTeamB = scoreTeamB + 3;
+    displayForTeamB(scoreTeamB);
+  }
+
+  /**
    * Displays the given score for Team A.
    */
   public void displayForTeamA(int score) {
     TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+    scoreView.setText(String.valueOf(score));
+  }
+
+  /**
+   * Displays the given score for Team B.
+   */
+  public void displayForTeamB(int score) {
+    TextView scoreView = (TextView) findViewById(R.id.team_b_score);
     scoreView.setText(String.valueOf(score));
   }
 }
